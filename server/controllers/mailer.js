de8fbc9a13f706b75f75f833bb04acea8966275c
Andrew Yeung <andrewhyeung@gmail.com>
2016-10-24 09:33:36 -0700

    implemented pw reset

--- server/controllers/mailer.js
+++ server/controllers/mailer.js
@@ -1,6 +1,16 @@
-var secret = require('./app.js')
+var mongoose = require('mongoose');
+var secret = require('./app.js');
+var crypto = require('crypto');
+var async = require('async');
+var User = mongoose.model('users');
+var bcrypt = require('bcryptjs');
+var secretPassword = function(password){
+    var salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt);
+}
 // nodemailer setup
 var nodemailer = require('nodemailer'); 
+
 var smtpConfig ={
   service: "Gmail",
   host: 'smtp.gmail.com', 
@@ -21,6 +31,7 @@ var transporter = nodemailer.createTransport(smtpConfig);
 module.exports = (function() {
   return {
     registration: function(req, res){
+
       console.log(req.body);
       var mailOptions = {
           from: 'FMFA@fmfa.org', 
@@ -41,20 +52,56 @@ module.exports = (function() {
     }, 
 
     forgotEmail: function(req, res){
-      var mailOptions = {
+      // crypto.randomBytes(20, function(err, buf) {
+      //   var token = buf.toString('hex');
+      // });
+      // var mailOptions = {
+      //     from: 'FMFA@fmfa.org', 
+      //     to: req.body.email, // list of receivers 
+      //     subject: 'FMFA Inc. Password Reset', // Subject line 
+      //     text:'You are receiving this because you (or someone else) have requested the reset of the password for your account. \n Please click on the following link, or paste this into your browser to complete the process: \n localhost:8000/#/reset/'+token+'\n If you did request this, please ignore this email and your password will remain unchanged.'        
+      // };
+      // console.log('this is the mail options', mailOptions); 
+
+      // transporter.sendMail(mailOptions, function(error, info){
+      //     if(error){
+      //         return console.log('this is the ', error);
+      //     }
+      //     console.log('Message sent: ' + info.response);
+      // });
+      crypto.randomBytes(8, function(err, buf) {
+        var token = buf.toString('hex');
+        console.log(token);
+        User.findOne({email:req.body.email}, function(err, user){
+            if (err) {
+                console.log(err);
+            } else {
+                user.password = secretPassword(token);
+                user.save(function(err){
+                  if(err){
+                    console.log('there was an error with pw reset')
+                    
+                  }
+                  res.json(user);
+                });
+            }
+        });
+        var mailOptions = {
           from: 'FMFA@fmfa.org', 
           to: req.body.email, // list of receivers 
           subject: 'FMFA Inc. Password Reset', // Subject line 
-          text:'You are receiving this because you (or someone else) have requested the reset of the password for your account. \n Please click on the following link, or paste this into your browser to complete the process: \n LINK \n If you did request this, please ignore this email and your password will remain unchanged.'        
-      };
-      console.log('this is the mail options', mailOptions); 
+          text:'You are receiving this because you (or someone else) have requested the reset of the password for your account. \n We have reset your password to ' + token + '. Please use this password to login and subsequently change your password'        
+        };
+        console.log('this is the mail options', mailOptions); 
 
-      transporter.sendMail(mailOptions, function(error, info){
-          if(error){
-              return console.log('this is the ', error);
-          }
-          console.log('Message sent: ' + info.response);
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                return console.log('this is the ', error);
+            }
+            console.log('Message sent: ' + info.response);
+        });
       });
+      
     }
   }
 })();
